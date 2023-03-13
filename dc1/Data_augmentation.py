694d530c614589ab0a5a75931b2265035bf79b13
snehaaaa09 <sneha.ajay9@gmail.com>
2023-03-13 20:42:42 +0100

    Did better horizontal and vertical shifts

--- dc1/Data_augmentation.py
+++ dc1/Data_augmentation.py
@@ -1,24 +1,16 @@
 # importing all the required libraries
 import warnings
-
 warnings.filterwarnings('ignore')
+
 import numpy as np
-import skimage.io as io
-from skimage.transform import rotate, AffineTransform, warp
-from skimage.util import random_noise
-from skimage.filters import gaussian
-import matplotlib.pyplot as plt
-from dc1.image_dataset import ImageDataset
+from PIL import Image
+from PIL.ImageTransform import AffineTransform
 
 # Torch imports
-import torch
-import torch.nn as nn
-import torch.optim as optim
 from torchsummary import summary  # type: ignore
-# poopy
-
 
 # Other imports
+from skimage.transform import warp
 import matplotlib.pyplot as plt  # type: ignore
 from matplotlib.pyplot import figure
 import os
@@ -34,38 +26,20 @@ from torchvision.datasets import MNIST
 
 # Loading the training datasets
 data_train_X = np.load('C:\GitHub\JBG040-Group10\data\X_train.npy')
-data_train_Y = np.load('C:\GitHub\JBG040-Group10\data\X_train.npy')
+data_train_Y = np.load('C:\GitHub\JBG040-Group10\data\Y_train.npy')
 
 # Loading the testing datasets
 data_test_X = np.load('C:\GitHub\JBG040-Group10\data\X_test.npy')
 data_test_Y = np.load('C:\GitHub\JBG040-Group10\data\Y_test.npy')
 
-# Left/right/up/down translatiion
-def translate(img, shift=10, direction='left', roll=True):
-    assert direction in ['right', 'left', 'down', 'up'], 'Directions should be top|up|left|right'
-    img = img.copy()
-    if direction == 'right':
-        right_slice = img[:, -shift:].copy()
-        img[:, shift:] = img[:, :-shift]
-        if roll:
-            img[:, :shift] = np.fliplr(right_slice)
-    if direction == 'left':
-        left_slice = img[:, :shift].copy()
-        img[:, :-shift] = img[:, shift:]
-        if roll:
-            img[:, -shift:] = left_slice
-    if direction == 'down':
-        down_slice = img[-shift:, :].copy()
-        img[shift:, :] = img[:-shift, :]
-        if roll:
-            img[:shift, :] = down_slice
-    if direction == 'up':
-        upper_slice = img[:shift, :].copy()
-        img[:-shift, :] = img[shift:, :]
-        if roll:
-            img[-shift:, :] = upper_slice
-    return img
 
-img = data_test_X[0][0]
-print(translate(img, direction='up', shift=20))
-print(translate(img, direction='down', shift=20))
+# Selecting the first image from test data for X
+img_numpy = data_test_X[0][0]
+image = Image.fromarray(img_numpy)
+
+# Creating the random shifting transformation
+transform = transforms.RandomAffine(degrees=0, translate=(0.2, 0), fill=0)
+
+# Apply the transformation to the image
+shifted_img = transform(image)
+shifted_img.show()
