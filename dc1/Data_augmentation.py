b1c81ac93ce9ec19bb32c3d1a98f1f2cb8333884
isha-narang <106186243+isha-narang@users.noreply.github.com>
2023-03-13 23:38:19 +0100

    Data augmentation - Combination transformations of 1. horizontal & vertical shift 2. Adjusting brightness 3. Adjusting sharpness 4. Random Rotation Gives output of two images - before and after data augmentation

--- dc1/Data_augmentation.py
+++ dc1/Data_augmentation.py
@@ -2,6 +2,9 @@
 import warnings
 warnings.filterwarnings('ignore')
 
+import random
+
+
 import numpy as np
 from PIL import Image
 from PIL.ImageTransform import AffineTransform
@@ -22,23 +25,32 @@ from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
 
 # Loading the training datasets
-data_train_X = np.load('C:\GitHub\JBG040-Group10\data\X_train.npy')
-data_train_Y = np.load('C:\GitHub\JBG040-Group10\data\Y_train.npy')
+data_train_X = np.load(r'C:\Users\ishik\OneDrive\Documents\GitHub\Vis final project\DataChallenge1\data\X_train.npy')
+data_train_Y = np.load(r'C:\Users\ishik\OneDrive\Documents\GitHub\Vis final project\DataChallenge1\data\Y_train.npy')
 
 # Loading the testing datasets
-data_test_X = np.load('C:\GitHub\JBG040-Group10\data\X_test.npy')
-data_test_Y = np.load('C:\GitHub\JBG040-Group10\data\Y_test.npy')
+data_test_X = np.load(r'C:\Users\ishik\OneDrive\Documents\GitHub\Vis final project\DataChallenge1\data\X_test.npy')
+data_test_Y = np.load(r'C:\Users\ishik\OneDrive\Documents\GitHub\Vis final project\DataChallenge1\data\X_train.npy')
 
 
 # Selecting the first image from test data for X
-img_numpy = data_test_X[0][0]
+img_numpy = data_test_X[1][0]
 image = Image.fromarray(img_numpy)
+image.show()
 
-# Creating the random shifting transformation
-transform = transforms.RandomAffine(degrees=0, translate=(0.2, 0), fill=0)
+# generating a random number to use as input for random adjust sharpener
+sharpness_factor = random.sample(range(0,3),1)[0]
+print(sharpness_factor)
+
+# Creating the brightness transformation
+transform = transforms.Compose([
+    transforms.ColorJitter(brightness=(0.5,1.5)),
+    transforms.RandomAffine(degrees=0, translate=(0.2, 0), fill=0),
+    transforms.RandomAdjustSharpness(sharpness_factor=sharpness_factor,p=0.3),
+    transforms.RandomRotation(degrees=(0,10))
+   ])
 
 # Apply the transformation to the image
 shifted_img = transform(image)
 shifted_img.show()
 
-#shit
