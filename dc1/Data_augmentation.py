4afcbfd7df1083a53e1c6816a85afbdb5c8be97f
snehaaaa09 <sneha.ajay9@gmail.com>
2023-03-14 14:51:20 +0100

    Correcting the sampling process to also include the indexes of the y dataset for training and saving X and Y datasets in "data" directory.

--- dc1/Data_augmentation.py
+++ dc1/Data_augmentation.py
@@ -1,16 +1,15 @@
 # importing all the required libraries
 import warnings
+
 warnings.filterwarnings('ignore')
 
+import torch
 import random
-
-
 import numpy as np
 from PIL import Image
 from PIL.ImageTransform import AffineTransform
 
 # Torch imports
-import torch
 from torchsummary import summary  # type: ignore
 
 # Other imports
@@ -20,59 +19,63 @@ from matplotlib.pyplot import figure
 import os
 import argparse
 import plotext  # type: ignore
-
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
 
+# Setting the seed
+np.random.seed(69)
+
 # Loading the training datasets
-data_train_X = np.load(r'C:\Users\ishik\OneDrive\Documents\GitHub\Vis final project\DataChallenge1\data\X_train.npy')
-data_train_Y = np.load(r'C:\Users\ishik\OneDrive\Documents\GitHub\Vis final project\DataChallenge1\data\Y_train.npy')
+data_train_X = np.load(r'C:\GitHub\JBG040-Group10\data\X_train.npy')
+data_train_Y = np.load(r'C:\GitHub\JBG040-Group10\data\Y_train.npy')
 
-# Loading the testing datasets
-data_test_X = np.load(r'C:\Users\ishik\OneDrive\Documents\GitHub\Vis final project\DataChallenge1\data\X_test.npy')
-data_test_Y = np.load(r'C:\Users\ishik\OneDrive\Documents\GitHub\Vis final project\DataChallenge1\data\X_train.npy')
+# Generating a random number to use as input for random adjust sharpener
+sharpness_factor = random.sample(range(0, 3), 1)[0]
 
-
-# Selecting the first image from test data for X
-# since the data_test_X is a numpy.nd array of only one column, the random function will pick values for which row (picture) to augment
-
-# create a loop and put everything down below in the loop; inlcude random number generator for the images and
-img_numpy = data_test_X[9][0] # change to data_train_X!!!!
-image = Image.fromarray(img_numpy)
-image.show()
-
-# generating a random number to use as input for random adjust sharpener
-sharpness_factor = random.sample(range(0,3),1)[0]
-# print(sharpness_factor)
-
-# Creating the brightness transformation
+# Creating and storing all the transformations
 transform = transforms.Compose([
-    transforms.ColorJitter(brightness=(0.5,1.5)),
-    transforms.RandomAffine(degrees=0, translate=(0.2, 0), fill=0),
-    transforms.RandomAdjustSharpness(sharpness_factor=sharpness_factor,p=0.3),
-    transforms.RandomRotation(degrees=(0,10))
-   ])
+   transforms.ColorJitter(brightness=(0.5, 1.5)),
+   transforms.RandomAffine(degrees=0, translate=(0.2, 0), fill=0),
+   transforms.RandomAdjustSharpness(sharpness_factor=sharpness_factor, p=0.3),
+   transforms.RandomRotation(degrees=(0, 10))
+])
 
-# Apply the transformation to the image
-shifted_img = transform(image)
-# shifted_img.show()
+# Sampling a predefined number of images from the training dataset using reservoir sampling
 
-# create an empty numpy array to store augmented images
-augmented_images = np.empty_like(data_test_X) # remember to change this to data_train_X
+# Create a list of indices corresponding to the images in the original dataset
+indices = list(range(len(data_train_X)))
 
-# iterate over the original images, apply the transformation pipeline and store the augmented images
-for idx, image in enumerate(data_test_X): # remember to change this to data_train_X
-    augmented_image = transform(torch.tensor(image)).numpy()
-    augmented_images[idx] = augmented_image
+# Setting the sample size
+k = 10000
 
-# concatenate the original and augmented images along the first axis
-all_images = np.concatenate((data_test_X, augmented_images), axis=0)
+# Use the reservoir sampling algorithm to sample k images from the dataset
+reservoir = data_train_X[:k].copy()
+sampled_indices = indices[:k].copy()
+for i in range(k, len(data_train_X)):
+   j = random.randint(0, i)
+   if j < k:
+       reservoir[j] = data_train_X[i].copy()
+       sampled_indices[j] = indices[i]
 
-print(len(all_images))
-for i in all_images:
-    print(i)
+# Storing the sampled X and the sampled Y
+sampled_X = reservoir
+sampled_Y = data_train_Y[sampled_indices]
 
-testing = Image.fromarray(all_images[9][0])
-testing.show()
+# Create an empty numpy array to store augmented images
+augmented_images = np.empty_like(sampled_X)
 
+# Iterate over the sampled images, apply the transformation pipeline and store the augmented images
+for idx, image in enumerate(sampled_X):
+   augmented_image = transform(torch.tensor(image)).numpy()
+   augmented_images[idx] = augmented_image
+
+# Concatenate the original and augmented images along the first axis for X dataset
+all_images = np.concatenate((data_train_X, augmented_images), axis=0)
+
+# Concatenate the original Y dataset with the class labels for the augmented data
+all_Y = np.concatenate((data_train_Y, sampled_Y), axis=0)
+
+# Saving the new X and Y training dataset
+np.save('C:/GitHub/JBG040-Group10/data/X_train_augmented.npy', all_images)
+np.save('C:/GitHub/JBG040-Group10/data/Y_train_augmented.npy', all_Y)
