cc71db7fef7ee0b0b5cf027948f1ae2e0377e4b8
deniz0zn <119049752+deniz0zn@users.noreply.github.com>
2023-03-27 18:12:05 +0100

    Add files via upload
    Implemented confusion matrix into main.py a visualization is given as an output for cm.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # Custom imports
-import numpy as np
-
 from dc1.batch_sampler import BatchSampler
 from dc1.image_dataset import ImageDataset
 from dc1.net import Net
 from dc1.train_test import train_model, test_model
-from Data_Augmentation import data_augmentation
+from early_stop import EarlyStopping
+from sklearn.metrics import confusion_matrix, classification_report
+import seaborn as sns
+
 
 # Torch imports
 import torch
@@ -23,44 +24,12 @@ from datetime import datetime
 from pathlib import Path
 from typing import List
 
-from sklearn.model_selection import train_test_split
-from imblearn.under_sampling import RandomUnderSampler
 
 def main(args: argparse.Namespace, activeloop: bool = True) -> None:
-    # Creating the augmented training dataset
-    print("The augmented training datasets are being prepared....")
-    data_augmentation()
 
-    # Loading the augmented datasets
-    X_train = np.load("data/X_train_augmented.npy")
-    Y_train = np.load("data/Y_train_augmented.npy")
-
-    # Split data into training and validation sets
-    X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size=0.5, random_state=42)
-
-    #Stratify data and flatten it to 2D arrays
-    rus = RandomUnderSampler(sampling_strategy='majority', random_state=42)
-    X_rus_train, Y_rus_train = rus.fit_resample(X_train.reshape(X_train.shape[0], -1), Y_train)
-    X_rus_val, Y_rus_val = rus.fit_resample(X_val.reshape(X_val.shape[0], -1), Y_val)
-
-    # Reshape the arrays into original shape
-    X_rus_train = X_rus_train.reshape(-1, 1, 128, 128)
-    X_rus_val = X_rus_val.reshape(-1, 1, 128, 128)
-    Y_rus_train = Y_rus_train.flatten()
-    Y_rus_val = Y_rus_val.flatten()
-
-    # Save split data to disk
-    np.save("data/X_train_split.npy", X_rus_train)
-    np.save("data/Y_train_split.npy", Y_rus_train)
-    np.save("data/X_val.npy", X_rus_val)
-    np.save("data/Y_val.npy", Y_rus_val)
-
-    # Create ImageDataset objects from the split data
-    train_dataset = ImageDataset(Path("data/X_train_split.npy"), Path("data/Y_train_split.npy"))
-    val_dataset = ImageDataset(Path("data/X_val.npy"), Path("data/Y_val.npy"))
-
-    # Loading test data
-    test_dataset = ImageDataset(Path("data/X_test.npy"), Path("data/Y_test.npy"))
+    # Load the train and test data set
+    train_dataset = ImageDataset(Path("../data/X_train.npy"), Path("../data/Y_train.npy"))
+    test_dataset = ImageDataset(Path("../data/X_test.npy"), Path("../data/Y_test.npy"))
 
     # Load the Neural Net. NOTE: set number of distinct labels here
     model = Net(n_classes=6)
@@ -87,69 +56,69 @@ def main(args: argparse.Namespace, activeloop: bool = True) -> None:
         # Creating a summary of our model and its layers:
         summary(model, (1, 128, 128), device=device)
     elif (
-            torch.backends.mps.is_available() and not DEBUG
+        torch.backends.mps.is_available() and not DEBUG
     ):  # PyTorch supports Apple Silicon GPU's from version 1.12
         print("@@@ Apple silicon device enabled, training with Metal backend...")
         device = "mps"
         model.to(device)
+
     else:
         print("@@@ No GPU boosting device found, training on CPU...")
         device = "cpu"
         # Creating a summary of our model and its layers:
         summary(model, (1, 128, 128), device=device)
 
-    # Let's now train, validate and test our model for multiple epochs:
+    # Lets now train, validate and test our model for multiple epochs:
     train_sampler = BatchSampler(
         batch_size=batch_size, dataset=train_dataset, balanced=args.balanced_batches
     )
     test_sampler = BatchSampler(
         batch_size=100, dataset=test_dataset, balanced=args.balanced_batches
     )
-    val_sampler = BatchSampler(
-        batch_size=100, dataset=val_dataset, balanced=args.balanced_batches
-    )
+    val_sampler = BatchSampler(batch_size=100, dataset=train_dataset, balanced=args.balanced_batches)
 
-    best_val_loss = float("inf")   # smallest valuation loss over epochs
-    patience = 5  # number of epochs to wait for improvement
-    wait = 0  # number of epochs without improvement
 
     mean_losses_train: List[torch.Tensor] = []
     mean_losses_test: List[torch.Tensor] = []
     mean_losses_val: List[torch.Tensor] = []
 
+    conf_matrix = torch.zeros((6, 6), dtype=torch.int64)
+
     for e in range(n_epochs):
         if activeloop:
+
             # Training:
-            losses = train_model(model, train_sampler, optimizer, loss_function, device)
-
+            train_losses = train_model(model, train_sampler, optimizer, loss_function, device)
             # Calculating and printing statistics:
-            mean_loss = sum(losses) / len(losses)
+            mean_loss = sum(train_losses) / len(train_losses)
             mean_losses_train.append(mean_loss)
-            print(f"\nEpoch {e + 1} training done, loss on train set: {mean_loss}/n")
-
-            # Validaton
-            losses = test_model(model, val_sampler, loss_function, device)
-
-            # Calculating and printing statistics:
-            mean_loss = sum(losses) / len(losses)
-            mean_losses_val.append(mean_loss)
-            print(f"\nEpoch {e + 1} validation done, loss on validation set: {mean_loss}\n")
-
-            ### Check for early stopping
-            if mean_loss < best_val_loss:
-                best_val_loss = mean_loss
-                wait = 0
-            else:
-                wait += 1
-                if wait >= patience:
-                    print(f"No improvement for {patience} epochs. Stopping early.")
-                    break
+            print(f"\nEpoch {e + 1} training done, loss on train set: {mean_loss}\n")
 
             # Testing:
-            losses = test_model(model, test_sampler, loss_function, device)
+            test_losses = test_model(model, test_sampler, loss_function, device)
+
+
+            y_true = []
+            y_pred = []
+            for batch in test_sampler:
+                X, y = batch
+                X = X.to(device)
+                y = y.to(device)
+                out = model(X)
+                pred = out.argmax(dim=1)
+                y_true.append(y.cpu())
+                y_pred.append(pred.cpu())
+                # Update confusion matrix
+                conf_matrix += confusion_matrix(y.cpu(), pred.cpu(), labels=[0, 1, 2, 3, 4, 5])
+
+            # Validation:
+            val_losses = test_model(model, val_sampler, loss_function, device)
+            mean_loss = sum(val_losses) / len(val_losses)
+            mean_losses_val.append(mean_loss)
+            print(f"\nEpoch {e + 1} validation done, loss on validation set{mean_loss}\n")
 
             # # Calculating and printing statistics:
-            mean_loss = sum(losses) / len(losses)
+            mean_loss = sum(test_losses) / len(test_losses)
             mean_losses_test.append(mean_loss)
             print(f"\nEpoch {e + 1} testing done, loss on test set: {mean_loss}\n")
 
@@ -157,53 +126,97 @@ def main(args: argparse.Namespace, activeloop: bool = True) -> None:
             plotext.clf()
             plotext.scatter(mean_losses_train, label="train")
             plotext.scatter(mean_losses_test, label="test")
-            plotext.scatter(mean_losses_val, label="val")
-            plotext.title("Train, Validation and Test loss")
+            plotext.title("Train and test loss")
 
             plotext.xticks([i for i in range(len(mean_losses_train) + 1)])
 
             plotext.show()
 
+            # early stopping
+            early_stopping = EarlyStopping(tolerance=5, min_delta=10)
+            early_stopping(train_losses[e], test_losses[e])
+            if early_stopping.early_stop:
+                print("We are at epoch:", e)
+                # retrieve current time to label artifacts
+                now = datetime.now()
+                # check if model_weights/ subdir exists
+                if not Path("model_weights/").exists():
+                    os.mkdir(Path("model_weights/"))
+
+                # Saving the model
+                torch.save(model.state_dict(),
+                           f"model_weights/model_{now.month:02}_{now.day:02}_{now.hour}_{now.minute:02}.txt")
+
+                # Create plot of test_losses
+                figure(figsize=(9, 10), dpi=80)
+                fig, (ax1, ax2) = plt.subplots(2, sharex=True)
+
+                ax1.plot(range(1, 1 + n_epochs), [x.detach().cpu() for x in mean_losses_train], label="Train",
+                         color="blue")
+                ax2.plot(range(1, 1 + n_epochs), [x.detach().cpu() for x in mean_losses_test], label="Test",
+                         color="red")
+                fig.legend()
+
+                # Check if /artifacts/ subdir exists
+                if not Path("artifacts/").exists():
+                    os.mkdir(Path("artifacts/"))
+
+                # save plot of test_losses
+                fig.savefig(Path("artifacts") / f"session_{now.month:02}_{now.day:02}_{now.hour}_{now.minute:02}.png")
+                break
+
+    print("Confusion matrix:")
+    print(conf_matrix)
+    # Plot the confusion matrix
+    sns.set(font_scale=1.4)
+    sns.heatmap(conf_matrix, annot=True, annot_kws={"size": 16}, cmap="Blues", fmt="d")
+    plt.title("Confusion Matrix")
+    plt.xlabel("Predicted Label")
+    plt.ylabel("True Label")
+    plt.show()
+
+
     # retrieve current time to label artifacts
     now = datetime.now()
-
     # check if model_weights/ subdir exists
     if not Path("model_weights/").exists():
         os.mkdir(Path("model_weights/"))
-
+    
     # Saving the model
     torch.save(model.state_dict(), f"model_weights/model_{now.month:02}_{now.day:02}_{now.hour}_{now.minute:02}.txt")
-
-    # Create plot of losses
+    
+    # Create plot of test_losses
     figure(figsize=(9, 10), dpi=80)
-    fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True)
+    fig, (ax1, ax2) = plt.subplots(2, sharex=True)
+    
+    ax1.plot(range(1, 1 + n_epochs), [x.detach().cpu() for x in mean_losses_train], label="Train", color="blue")
+    ax2.plot(range(1, 1 + n_epochs), [x.detach().cpu() for x in mean_losses_test], label="Test", color="red")
 
-    ax1.plot(range(1, len(mean_losses_train)+1), [x.detach().cpu() for x in mean_losses_train], label="Train", color="blue")
-    ax2.plot(range(1, len(mean_losses_test)+1), [x.detach().cpu() for x in mean_losses_test], label="Test", color="red")
-    ax3.plot(range(1, len(mean_losses_val)+1), [x.detach().cpu() for x in mean_losses_val], label="Validation", color="green")
     fig.legend()
-
+    
     # Check if /artifacts/ subdir exists
     if not Path("artifacts/").exists():
         os.mkdir(Path("artifacts/"))
 
-    # save plot of losses
-    fig.savefig(Path("artifacts") / f"session_{now.month:02}_{now.day: 02}_{now.hour}_{now.minute:02}.png")
+    # save plot of test_losses
+    fig.savefig(Path("artifacts") / f"session_{now.month:02}_{now.day:02}_{now.hour}_{now.minute:02}.png")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "--nb_epochs", help="number of training iterations", default=100, type=int
+        "--nb_epochs", help="number of training iterations", default=2, type=int
     )
     parser.add_argument("--batch_size", help="batch_size", default=50, type=int)
+    parser.add_argument("--val_size", help="size of the validation set", default=0.10, type = float)
     parser.add_argument(
         "--balanced_batches",
         help="whether to balance batches for class labels",
-        default=False,
+        default=True,
         type=bool,
     )
     args = parser.parse_args()
 
     main(args)
+
